60462fc730784fa747317ba630e8e8425521ea1e
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-04-23 17:13:55 +0200

    Fixato il costruttore di DatiAttivazione in SkipassGiornaliero

--- PrimoParziale/SkipassGiornaliero/src/DatiAttivazione.java
+++ PrimoParziale/SkipassGiornaliero/src/DatiAttivazione.java
@@ -4,7 +4,14 @@ public class DatiAttivazione {
 	private String nome;
 	
 	public DatiAttivazione(int giorno, String nome) {
-		this.giorno = giorno;
+		//controllo che il giorno sia valido
+		if(giorno > 0 && giorno <= 365) {
+            this.giorno = giorno;
+	    }
+	    else {
+	            this.giorno = 1;
+	    }
+		
 		this.nome = nome;
 	}
 	
