f12b5f0b291883e5966c9cae290e11166bd83248
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-04-23 18:30:54 +0200

    Implementato il costruttore di CasaIndipendente e getIndirizzo

--- PrimoParziale/CasaIndipendente/src/CasaIndipendente.java
+++ PrimoParziale/CasaIndipendente/src/CasaIndipendente.java
@@ -1,8 +1,24 @@
 
 public class CasaIndipendente {
 	private String indirizzo;
+	private Locale[] locali;
 	
 	public CasaIndipendente(String indirizzo, int maxLocali) {
+		if(maxLocali > 0) {
+			locali = new Locale[maxLocali];
+		}
+		else {
+			locali = new Locale[1];
+		}
 		
+		if(indirizzo != null) {
+			this.indirizzo = indirizzo;
+		}
 	}
+	
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	
+	//continuare qui
 }
