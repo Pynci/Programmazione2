3e31f9b12fd23643d6708d6b31332389cf0e60d3
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-04-26 19:41:54 +0200

    Scrittura della classe Auto

---
+++ PrimoParziale/ParcoAuto/src/Auto.java
@@ -0,0 +1,53 @@
+
+public class Auto {
+	private String targa;
+	private int km;
+	
+	public Auto(String targa, int kmPercorsi) {
+		this.targa = targa;
+		km = kmPercorsi;
+	}
+	
+	public Auto(String targa) {
+		this(targa,0);
+	}
+	
+	public boolean aggiornaKm(int kmAggiuntivi) {
+		if(kmAggiuntivi >= 0) {
+			km += kmAggiuntivi;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public String getTarga() {
+		return targa;
+	}
+
+	public int getKm() {
+		return km;
+	}
+	
+	public boolean equals(Auto altra) {
+		if(altra == null) {
+			return false;
+		}
+		else if(this == altra) {
+			return true;
+		}
+		else if(this.targa.equals(altra.targa)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public String toString() {
+		return "targa: " + targa + " km: " + km;
+	}
+	
+	
+}
