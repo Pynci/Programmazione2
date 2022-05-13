a58c3b8dc8ed462d47ca4a674eac93505505019f
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-05-13 20:43:55 +0200

    Prima scrittura della classe prodotto (ancora da verificare)

--- E05/CDMusicale/src/prodotti/Prodotto.java
+++ E05/CDMusicale/src/prodotti/Prodotto.java
@@ -1,5 +1,45 @@
 package prodotti;
 
 public class Prodotto {
+	private double prezzo;
+	private double sconto;
+	private String nome;
+	
+	public Prodotto(String nome, double prezzo, double sconto) {
+		this.nome = nome;
+		this.prezzo = prezzo;
+		this.sconto = sconto;
+	}
 
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public double getSconto() {
+		return sconto;
+	}
+
+	public void setSconto(double sconto) {
+		this.sconto = sconto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double calcolaPrezzo() {
+		return prezzo - sconto;
+	}
+	
+	public String toString() {
+		return "[nome:" + nome + "; prezzo: " + prezzo + "; sconto: " + sconto + ";]";
+	}
 }
