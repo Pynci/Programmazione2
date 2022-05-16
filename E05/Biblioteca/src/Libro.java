25be20245462d8a7ff83c49dfda5d11967d3ff87
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-05-16 20:49:23 +0200

    Creato il progetto "Biblioteca" e importate le classi Libro e Prodotto

---
+++ E05/Biblioteca/src/Libro.java
@@ -0,0 +1,47 @@
+public class Libro extends Prodotto {
+	private String autore;
+	protected String[] pagine;
+	
+	public Libro(String nome, double prezzo, double sconto, String autore, String[] pagine) {
+		//controlli da implementare
+		super(nome, prezzo, sconto);
+		this.autore = autore;
+		this.pagine = pagine;
+	}
+
+	public String getAutore() {
+		return autore;
+	}
+
+	public int getNumPagine() {
+		int numPagine = 0;
+		
+		for(int i = 0; i < pagine.length; i++) {
+			if(pagine[i] != null) {
+				numPagine++;
+			}
+		}
+		
+		return numPagine;
+	}
+	
+	public String toString() {
+		return super.toString() + "[autore:" + autore + "][numPagine:" + getNumPagine() + "]";
+	}
+	
+	public String sfoglia() {
+		String lettura = "";
+		
+		if(pagine != null) {
+			for(int i = 0; i < pagine.length; i++) {
+				if(pagine[i] != null && !pagine[i].isEmpty()) {
+					lettura += "\n" + pagine[i];
+				}
+			}	
+		}
+		
+		return lettura;
+	}
+	
+	
+}
---
+++ E05/Biblioteca/src/Prodotto.java
@@ -0,0 +1,44 @@
+public class Prodotto {
+	private double prezzo;
+	private double sconto;
+	private String nome;
+	
+	public Prodotto(String nome, double prezzo, double sconto) {
+		//eventuali controlli ancora da implementare
+		this.nome = nome;
+		this.prezzo = prezzo;
+		this.sconto = sconto;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public double getSconto() {
+		return sconto;
+	}
+
+	public void setSconto(double sconto) {
+		this.sconto = sconto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double calcolaPrezzo() {
+		return prezzo - (prezzo * sconto) / 100;
+	}
+	
+	public String toString() {
+		return "[nome:" + nome + "][prezzo:" + prezzo + "][sconto:" + sconto + "]";
+	}
+}
