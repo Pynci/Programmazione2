89ad3ecc630a2508794a3c759926a3d632c48fc9
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-06-16 09:12:03 +0200

    Fix della directory

---
+++ E09/LibroLibreria/src/verArrayList/LibreriaException.java
@@ -0,0 +1,13 @@
+package verArrayList;
+
+public class LibreriaException extends Exception {
+
+	public LibreriaException() {
+		super();
+	}
+
+	public LibreriaException(String message) {
+		super(message);
+	}
+	
+}
---
+++ E09/LibroLibreria/src/verArrayList/LibroException.java
@@ -0,0 +1,13 @@
+package verArrayList;
+
+public class LibroException extends Exception {
+
+	public LibroException() {
+		super();
+	}
+
+	public LibroException(String message) {
+		super(message);
+	}
+	
+}
