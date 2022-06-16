2db45fe3334e9e41d143918c8eadaaa5ed50def3
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-06-16 10:29:17 +0200

    Scritta e verificata la versione dell'esercizio con ArrayList

--- E09/LibroLibreria/src/verArrayList/Libreria.java
+++ E09/LibroLibreria/src/verArrayList/Libreria.java
@@ -1,6 +1,7 @@
 package verArrayList;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Libreria {
 	private String nome;
@@ -14,5 +15,40 @@ public class Libreria {
 			throw new LibreriaException("ATTENZIONE: nome non valido");
 		}
 		libri = new ArrayList<Libro>();
+	}
+	
+	public boolean aggiungiLibro(Libro nuovo) {
+		if(libri.contains(nuovo)) {
+			return false;
+		}
+		else {
+			return libri.add(nuovo);
+		}
+	}
+	
+	public int quantitaLibri() {
+		return libri.size();
+	}
+	
+	public List<Libro> rimuoviLibriAnno(int anno) {
+		List<Libro> rimossi = new LinkedList<Libro>();
+		for(Libro libro: libri) {
+			if(libro.getAnno() == anno) {
+				rimossi.add(libro);
+			}
+		}
+		for(Libro rimosso: rimossi) {
+			libri.remove(rimosso);
+		}
+		return rimossi;
+	}
+	
+	public String toString() {
+		String stringa = "{\n";
+		for(Libro libro: libri) {
+			stringa += libro.toString() + "\n";
+		}
+		stringa += "}";
+		return stringa;
 	}
 }
