a0cf9db78f55ddd78be1f07bcad5fed64c517c86
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-06-16 15:44:17 +0200

    Commit di fix rapido

---
+++ SecondoParziale/VoloDirettoMultiTratta/src/Volo.java
@@ -0,0 +1,11 @@
+
+public abstract class Volo {
+	
+	private String aeroportoPartenza;
+	private String aeroportoArrivo;
+	
+	public Volo(String aeroportoPartenza, String aeroportoArrivo) {
+		
+	}
+
+}
