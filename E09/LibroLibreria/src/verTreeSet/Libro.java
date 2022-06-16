b66eae23929c8bfecb71413eec00a537924e0a7a
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-06-16 10:31:01 +0200

    Fixata la classe libro nelle varie versioni

--- E09/LibroLibreria/src/verTreeSet/Libro.java
+++ E09/LibroLibreria/src/verTreeSet/Libro.java
@@ -1,12 +1,14 @@
 package verTreeSet;
 
+import java.util.Objects;
+
 public class Libro {
 	private String autore;
 	private String titolo;
-	private String anno;
+	private int anno;
 	private int numeroVolume;
 	
-	public Libro(String autore, String titolo, String anno) throws LibroException {
+	public Libro(String autore, String titolo, int anno) throws LibroException {
 		if(autore != null && !autore.isEmpty()) {
 			this.autore = autore;
 		}
@@ -21,15 +23,10 @@ public class Libro {
 			throw new LibroException("ATTENZIONE: titolo non valido");
 		}
 		
-		if(anno != null && !anno.isEmpty()) {
-			this.anno = anno;
-		}
-		else {
-			throw new LibroException("ATTENZIONE: anno non valido");
-		}
+		this.anno = anno;
 	}
 	
-	public Libro(String autore, String titolo, String anno, int numeroVolume) throws LibroException {
+	public Libro(String autore, String titolo, int anno, int numeroVolume) throws LibroException {
 		this(autore, titolo, anno);
 		
 		if(numeroVolume > 0) {
@@ -53,12 +50,25 @@ public class Libro {
 		return titolo;
 	}
 
-	public String getAnno() {
+	public int getAnno() {
 		return anno;
 	}
 
 	public int getNumeroVolume() {
 		return numeroVolume;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Libro other = (Libro) obj;
+		return Objects.equals(anno, other.anno) && Objects.equals(autore, other.autore)
+				&& Objects.equals(titolo, other.titolo);
 	}
 	
 	
